6dbcdca5367c4f65d3a4be24fa6ccc83468a517f
boboIqiqi <120148441@qq.com>
2017-07-22 16:47:43 +0800

    开始学习第6章，读入了一个csv文件。

---
+++ Ch06.R
@@ -0,0 +1,8 @@
+#导入CVS
+
+#tomato <- read.table(file = "data/TomatoFirst.csv", header = TRUE, sep = ",")
+tomato <- read.table("data/TomatoFirst.csv", header = TRUE, sep = ",")
+
+tomato
+
+head(tomato)
