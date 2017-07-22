33879ba3eb7fd6961051b2908767fcc195009bff
boboIqiqi <120148441@qq.com>
2017-07-22 17:28:34 +0800

    第六章也学完了。主要学习了一个函数: read.table 和一个参数：stringsAsFactors

--- Ch06.R
+++ Ch06.R
@@ -6,3 +6,16 @@ tomato <- read.table("data/TomatoFirst.csv", header = TRUE, sep = ",")
 tomato
 
 head(tomato)
+
+#下面主要介绍一下 stringsAsFactors这个选项
+
+x <- 10:1
+y <- -4:5
+q <- c("Hotkey", "Football", "Baseball", "Curling", "Rugby", 
+       "Lacrosse", "Basketball", "Tennis", "Cricket", "Soccer")
+
+theDF <- data.frame(First = x, Second = y, Sport = q, stringsAsFactors = FALSE)
+theDF$Sport
+
+theDF <- data.frame(First = x, Second = y, Sport = q)
+theDF$Sport
